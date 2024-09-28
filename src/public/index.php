d5e9558c50c4d77978c600820aec186652042513
Ariana <dondupova1997@mail.ru>
2024-09-28 23:40:39 +0800

    I do everything than i can

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,25 @@
 <?php
+
+$conn = pg_connect("host=postgres_db dbname=mydb user=user password=pass");
+if ($conn) {
+    echo "Connected to PostgreSQL successfully!";
+} else {
+    echo "Connection failed.";
+} //*/
+
+echo "<br>" . "\n";
+
 echo "Hello, World!";
-echo "\n";
-echo "I am a cool junior PHP backend-developer";
-echo "\n";
-echo "Я всё смогу!!!!!";
+echo "<br>" . "\n";
+echo "I am a super cool junior PHP backend-developer";
+echo "<br>" . "\n";
+echo "I will can everything!!!!!";
+
+echo "<br>" . "\n";
+
+echo " I will do OK!!!";
+echo "<br>" . "\n";
+echo "I finally will be a junior PHP Backend-developer!!! I just need to not give up!";
+echo "<br>" . "\n";
+echo "Я МОЛОДЕЦ!!! Я суперкрута!!! ВАААААУУУУУУ)))))))))";
 ?>
