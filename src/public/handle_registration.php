2747d44eaa4ccde6e33f3b3fcc4a40b8650d4e40
Ariana <dondupova1997@mail.ru>
2024-09-29 19:21:24 +0800

    удалила комментарий

--- src/public/handle_registration.php
+++ src/public/handle_registration.php
@@ -2,7 +2,7 @@
 $name = $_GET["name"];
 $email = $_GET["email"];
 $password = $_GET["psw"];
-//$passRepeat = $_GET["psw-repeat"];
+$passRepeat = $_GET["psw-repeat"];
 
 $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
 $pdo->exec("INSERT INTO users (name, email, password) VALUES ('{$name}', '{$email}', '{$password}')");
