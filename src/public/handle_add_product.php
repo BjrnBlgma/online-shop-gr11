fbcafe7b19f4765a0cfea5976349c034ab4d3aad
Ariana <dondupova1997@mail.ru>
2024-10-09 01:31:55 +0800

    плохо понимаю работу session_start...и из-за были ошибки, вроде непонятные вылеты были((

--- src/public/handle_add_product.php
+++ src/public/handle_add_product.php
@@ -1,73 +1,75 @@
 <?php
-if (!isset($_SESSION['user_id'])){
-    header('Location: /login');
-}
+session_start(); // session_start должен быть в начале
 
+if (!isset($_SESSION['user_id'])) {
+    header('Location: /login');
+    exit;
+}
 $errors = [];
+
 if (isset($_POST['product_id'])) {
-    $productId = htmlspecialchars($_POST['product_id'], ENT_QUOTES, 'UTF-8');
+    $productId = $_POST['product_id'];
     if (empty($productId)) {
         $errors['product'] = "ID товара не должно быть пустым";
-    }elseif (!ctype_digit($productId)) {
+    } elseif (!ctype_digit($productId)) {
         $errors['product'] = "Поле ID товара должно содержать только цифры!";
-    }elseif ($productId <= 0){
+    } elseif ($productId <= 0) {
         $errors['product'] = "Поле ID товара не должно содержать отрицательные значения";
     }
-} /*else {
-    $errors['product    '] = 'Выберите продкут';
-}*/
+} else {
+    $errors['product'] = 'Выберите продукт';
+}
 
 if (isset($_POST['amount'])) {
-    $amount = htmlspecialchars($_POST['amount'], ENT_QUOTES, 'UTF-8');
+    $amount = $_POST['amount'];
     if (empty($amount)) {
         $errors['amount'] = "Выберите количество товара";
-    }elseif (!ctype_digit($amount)) {
+    } elseif (!ctype_digit($amount)) {
         $errors['amount'] = "Поле количества товара должно содержать только цифры!";
-    }elseif ($amount <= 0){
-        $errors['amount'] = "Поле количества товара не должно быть отрицательным";
+    } elseif ($amount <= 0) {
+        $errors['amount'] = "Количество товара не должно быть отрицательным";
     }
-} /*else {
+} else {
     $errors['amount'] = 'Выберите количество товара';
-}*/
-function addProduct($user ,$product, $amount)
-{
+}
+
+function addProduct($user, $product, $amount) {
     $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
     $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES (:user_id, :product_id, :amount)");
     $stmt->execute(['user_id' => $user, 'product_id' => $product, 'amount' => $amount]);
     header('Location: /cart');
+    exit;
 }
-function plusAmount($user ,$product, $amount)
-{
+function plusAmount($user, $product, $amount) {
     $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
-    $stmt =$pdo->prepare("UPDATE user_products SET amount = :amount WHERE user_id = :userID AND product_id = :productID");
-    $stmt->execute(['userID' => $user, 'productID' => $product, 'amount' => $amount]);
+    $stmt = $pdo->prepare("UPDATE user_products SET amount = :amount WHERE user_id = :user_id AND product_id = :product_id");
+    $stmt->execute(['amount' => $amount, 'user_id' => $user, 'product_id' => $product]);
     header('Location: /cart');
+    exit;
 }
 
 if (empty($errors)) {
-    $productId = htmlspecialchars($_POST['product_id'], ENT_QUOTES, 'UTF-8');
-    $amount = htmlspecialchars($_POST['amount'], ENT_QUOTES, 'UTF-8');
-    session_start();
     $userId = $_SESSION['user_id'];
     $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
 
-
-    $stmt = $pdo->prepare('SELECT id FROM products WHERE id = :productID');
-    $stmt->execute(['productID' => $productId]);
+    $stmt = $pdo->prepare('SELECT id FROM products WHERE id = :product_id');
+    $stmt->execute(['product_id' => $productId]);
     $isCorrectIdProduct = $stmt->fetch(); //есть ли такой товар к продуктах
+
     if ($isCorrectIdProduct === false) {
         $errors['product'] = "Введите корректный ID товара";
-    }
-
-    $stmt = $pdo->prepare('SELECT * FROM user_products WHERE user_id = :userID AND product_id = :productID');
-    $stmt->execute(['userID' => $userId, 'productID' => $productId]);
-    $isProductInCart = $stmt->fetch();  //есть ли продукт в козрине или нет
-    if ($isProductInCart === false){
-        addProduct($userId, $productId, $amount);
     } else {
-        plusAmount($userId, $productId, $amount + $isProductInCart['amount']);
-    }
+        $stmt = $pdo->prepare('SELECT * FROM user_products WHERE user_id = :user_id AND product_id = :product_id');
+        $stmt->execute(['user_id' => $userId, 'product_id' => $productId]);
+        $isProductInCart = $stmt->fetch(); //есть ли продукт в козрине или нет
 
+        if ($isProductInCart === false) {
+            addProduct($userId, $productId, $amount); // Добавляем товар
+        } else {
+            $newAmount = $amount + $isProductInCart['amount'];
+            plusAmount($userId, $productId, $newAmount);
+        }
+    }
 }
 
 require_once 'get_add_product.php';
