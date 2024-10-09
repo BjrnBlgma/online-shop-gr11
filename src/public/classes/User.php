f3f4fb976c6a04c761c6fec238181f54db5654dc
Ariana <dondupova1997@mail.ru>
2024-10-09 17:22:57 +0800

    class для USER с проверкой на неправильные данные..вроде норм

--- src/public/classes/User.php
+++ src/public/classes/User.php
@@ -9,7 +9,6 @@ class User
             $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
             $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
             $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
-            $passRepeat = htmlspecialchars($_POST['psw-repeat'], ENT_QUOTES, 'UTF-8');
 
             try {
                 $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
@@ -18,18 +17,13 @@ class User
                 $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
                 $stmt->execute(['name' => $name, 'email' => $email, 'password' => $hash]);
 
-
-
-                #$stmt = $pdo->prepare("SELECT * FROM users WHERE email =  :email");
-                #$stmt->execute(['email' => $email]);
-                #print_r($stmt->fetch());
-                $result = 'Вы успешно зарегестрировались!';
-
                 header('Location: /login');
             } catch (PDOException $e) {
                 print "Error!: " . $e->getMessage();
                 //die();
             }
+        } else{
+            return $errors;
         }
     }
 
@@ -38,16 +32,15 @@ class User
     private function validate(): array
     {
         $errors = [];
+
         if (isset($_POST['name'])) {
             $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
             if (empty($name)){
-                $errors ['name'] = "Имя не должно быть пустым";
-            }elseif (strlen($name) < 2){
-                $errors ['name'] = "Слишком короткое имя";
-            }elseif (strlen($name) > 20){
-                $errors ['name'] = "Слишком длинное имя";
+                $errors['name'] = "Имя не должно быть пустым";
+            } elseif (strlen($name) < 3 || strlen($name) > 20) {
+                $errors['name'] = "Имя должно содержать не меньше 3 символов и не больше 20 символов";
             } elseif (!preg_match("/^[a-zA-Zа-яА-Я]+$/u", $name)) {
-                $errors ['name'] = "Имя может сожержать только буквы и пробелы";
+                $errors['name'] = "Имя может содержать только буквы";
             }
         }else{
             $errors ['name'] = "Поле name должно быть заполнено";
@@ -56,12 +49,19 @@ class User
 
         if (isset($_POST['email'])) {
             $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+
+            $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
+            $stmt = $pdo->prepare('SELECT email FROM users WHERE email = :email');
+            $stmt->execute(['email' => $email]);
+            $isCorrectEmail = $stmt->fetch(); //свободна ли почта или уже занята
             if (empty($email)) {
                 $errors ['email'] = "Почта не должна быть пустой";
             }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $errors ['email'] = "Введите корректный email-адрес!";
-            }elseif (strlen($email) < 3 && strlen($email) > 60) {
+            }elseif ( (strlen($email) < 3)||(strlen($email) > 60) ){
                 $errors ['email'] = "email-адрес должен содержать не меньше 3 символов и не больше 60 символов";
+            }elseif ($isCorrectEmail) {
+                $errors['email'] = "email уже занят!";
             }
         }else{
             $errors ['email'] = "Поле email должно быть заполнено";
@@ -72,7 +72,7 @@ class User
             $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
             if (empty($password)) {
                 $errors ['password'] = "Пароль не должен быть пустым";
-            }elseif (strlen($password) < 8 && strlen($password) > 20) {
+            }elseif (strlen($password) < 8 || strlen($password) > 20) {
                 $errors ['password'] = "Пароль должен быть не менее 8 символов и не более 20 символов";
             }
         }else{
@@ -82,14 +82,16 @@ class User
 
         if (isset($_POST['psw-repeat'])) {
             $passRepeat = htmlspecialchars($_POST['psw-repeat'], ENT_QUOTES, 'UTF-8');
+            $password = $_POST['password'];
             if (empty($passRepeat)) {
-                $errors ['psw-repeat'] = "Пароль не должен быть пустым";
+                $errors ['psw-repeat'] = "Поле не должно быть пустым";
             }elseif ($passRepeat != $password) {
                 $errors ['psw-repeat'] = "Пароли не совпадают"; //Passwords do not match"
             }
         }else {
             $errors ['psw-repeat'] = "Поле repeat password должно быть заполнено";
         }
+
         return $errors;
     }
 }
--- src/public/handle_registration.php
+++ src/public/handle_registration.php
@@ -3,5 +3,13 @@ require_once __DIR__ . "/classes/User.php";
 
 $user = new User();
 $user->register();
+
+$err = $user->register();
+if (!empty($err)) {
+    print_r($err);
+}
+/*foreach ($err as $key => $el) {
+    echo $key . "<br>" . "\n";
+}*/
 require_once "./get_registration.php";
 ?>
