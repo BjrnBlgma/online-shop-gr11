c9386740404b69de2281831b39b1cd43184707e9
Ariana <dondupova1997@mail.ru>
2024-10-15 01:17:28 +0800

    доделала формы заказа

--- src/Model/Order.php
+++ src/Model/Order.php
@@ -1,21 +1,28 @@
 <?php
+require_once "./../Database/Database.php";
+
 
 class Order
 {
+    private $pdo;
+    private Database $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->pdo = $this->db->getConnectPdo();
+    }
+
     public function createOrderId($name, $family, $city, $address, $phone, $email, $userId) //добавить переменные для добавления информации в табицу Order
     {
-        $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
-
-        $stmt = $pdo->prepare("INSERT INTO orders (name, family, city, address, phone, email, user_id) VALUES (:name, :family, :city, :address, :phone, :email, :user_id)"); //добавить переменные для добавления информации в табицу Order
+        $stmt = $this->pdo->prepare("INSERT INTO orders (name, family, city, address, phone, email, user_id) VALUES (:name, :family, :city, :address, :phone, :email, :user_id)"); //добавить переменные для добавления информации в табицу Order
         $stmt->execute(['name' => $name, 'family' => $family, 'city'=> $city, 'address'=> $address, 'phone'=> $phone, 'email' => $email, 'user_id'=> $userId]);
     }
 
 
     public function getByUserIdToTakeOrderId($userId)
     {
-        $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
-
-        $stmt = $pdo->prepare('SELECT * FROM orders WHERE user_id = :user_id');
+        $stmt = $this->pdo->prepare('SELECT * FROM orders WHERE user_id = :user_id');
         $stmt->execute(['user_id' => $userId]);
         $sendOrderId = $stmt->fetch();
 
--- src/Model/OrderProduct.php
+++ src/Model/OrderProduct.php
@@ -1,11 +1,20 @@
 <?php
+require_once "./../Database/Database.php";
 
 class OrderProduct
 {
+    private $pdo;
+    private Database $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->pdo = $this->db->getConnectPdo();
+    }
+
     public function sendProductToOrder(int $orderId, int $product, int $amount, int $price)
     {
-        $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
-        $stmt = $pdo->prepare("INSERT INTO order_products (order_id, product_id, amount, price) VALUES (:order_id, :product_id, :amount, :price)");
+        $stmt = $this->pdo->prepare("INSERT INTO order_products (order_id, product_id, amount, price) VALUES (:order_id, :product_id, :amount, :price)");
         $stmt->execute(['order_id' => $orderId, 'product_id' => $product, 'amount' => $amount, 'price'=> $price]);
     }
 }
